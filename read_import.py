aafdc0526c2aada96523eb22c7d0fe5a5db108de
Raidercol <davi.nunes@edu.pucrs.br>
2023-04-28 16:52:02 -0300

    doc: add the comentation in code

--- read_import.py
+++ read_import.py
@@ -3,22 +3,26 @@ from kpcon.Connections import connections as kpc
 import os 
 from sqlalchemy import text
 
-# conection com o banco
+# conect to the database
 conn = kpc.postgresql()
 
+# path and files
 PATH = os.path.dirname(os.path.abspath(__file__))
-# PATH = os.getcwd()
 FILES_PATH = os.path.join(PATH, 'files')
 
+# querys used in this script
 QUERY = "select * from agentes_autonomos.agente"
 QUERY_DEL = "delete from agentes_autonomos.agente"
 QUERY_FINAL = "select email from agentes_autonomos.agente where email is not NULL and email <> ' - '"
 
+# read the excel files in the folder files
 df_orama = pd.read_excel(os.path.join(FILES_PATH, "ORAMA.xlsx" ))
 df_genial = pd.read_excel(os.path.join(FILES_PATH, "GENIAL.xlsx" ))
 df_btg = pd.read_excel(os.path.join(FILES_PATH, "btg.xlsx" ))
 df_guide = pd.read_excel(os.path.join(FILES_PATH, "guide.xlsx" ))
 
+# change the columns names to the same name of the table in the database
+# add the column origem to know where the data came from and delete the columns that are not necessary
 df_genial.columns = ["email"]
 df_genial['origem'] = 'genial'
 
@@ -32,16 +36,21 @@ df_btg['origem'] = 'btg'
 df_guide = df_guide.drop(columns=['bairro', 'contratacao:'])
 df_guide.columns = ["nome_empresa", "endereco", "cep", "uf", "cidade", 'telefone', 'documento', 'email', 'site' , "nome_agente", 'origem']
 
+# select the data from the database
 agentes_db = pd.read_sql(text(QUERY), conn)
 
+# concat the data from the database with the data from the excel files deleting the duplicates
 agentes = pd.concat([agentes_db,df_genial, df_orama, df_btg, df_guide])
 agentes.drop_duplicates(inplace=True)
 
+# clean the data in uf column
 agentes['uf'] = agentes['uf'].apply(lambda x: str(x).replace(" ", "").replace("nan", ""))
 
+# delete the data from the database and insert the new data
 conn.execute(text(QUERY_DEL))
 
 agentes.to_sql("agente", con=conn, schema="agentes_autonomos", index=False, index_label=False, if_exists="append")
 conn.commit()
 
+# get the emails from the database and save in a excel file
 pd.read_sql(text(QUERY_FINAL), conn).to_excel(os.path.join(FILES_PATH, "emails.xlsx"), index=False)
--- separa_email.py
+++ separa_email.py
@@ -8,12 +8,16 @@ import datetime
 # absolute path and files
 PATH = os.path.dirname(os.path.abspath(__file__))
 PATH_FILES = os.path.join(PATH, "files")
-OUTPUT_PATH = os.path.join(PATH_FILES,"Output.txt")
+OUTPUT_PATH = os.path.join(PATH_FILES,"emails.txt")
 
+# conect to the database
 conn = kpc.postgresql()
 
+# start the jvm to use the asposecells
 jpype.startJVM()
 from asposecells.api import Workbook
+
+# convert the excel file to txt
 workbook = Workbook(os.path.join(PATH_FILES, "emails.xlsx"))
 workbook.save(OUTPUT_PATH)
 
@@ -23,18 +27,23 @@ dictio = {
     'emails' : []
 }
 
+# read the txt file and clean the data 
 with open(OUTPUT_PATH) as f:
     readed = f.read()
     f.close()
     
 readed = readed.replace(" ; ", "\n").replace("; ", "\n").replace(" ;", "\n").replace(";", "\n").replace("|", "\n").replace(".br ", ".br\n").replace("\n\n", "\n")
+
+# split the data in lines and delete the first and last line that are not necessary
 readed = readed.splitlines()
 readed.pop()
 readed.pop(0)
 
+# last clean in the data
 readed = (lambda x : [i.lower().strip("<> '/\n") for i in x])(readed)
 dictio['emails'] = readed
 
+# transform the data in a dataframe and insert in the database
 df = pd.DataFrame(dictio)
 df['data_insercao'] = datetime.date.today().strftime("%Y-%m-%d")
 
